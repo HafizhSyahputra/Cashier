13cf9f3cdf981597db462905f9e20e3112ce52e3
HafizhSyahputra <hafizhsyaputra676@gmail.com>
2023-10-11 14:34:19 +0700

    Create Stock Service Class

--- src/test/java/com/smk/cashier/service/BarangServiceTest.java
+++ src/test/java/com/smk/cashier/service/BarangServiceTest.java
@@ -1,11 +1,13 @@
 package com.smk.cashier.service;
 
+import com.smk.cashier.dao.BarangDao;
 import com.smk.cashier.model.Barang;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
+import java.util.Date;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -27,25 +29,46 @@ class BarangServiceTest {
     }
 
     @Test
+    @Order(4)
+    void saveBarangToDatabase(){
+        BarangDao barangDao = new BarangDao();
+        Barang laptop = new Barang();
+        laptop.setKodeBarang("LP001");
+        laptop.setNamaBarang("Laptop");
+        laptop.setHargaBarang(5000000);
+        laptop.setDateCreated(new Date());
+        laptop.setLastModified(new Date());
+        barangDao.save(laptop);
+
+
+    }
+
+    @Test
     @Order(1)
     void addBarang() {
         Barang laptop = new Barang();
         laptop.setKodeBarang("LP001");
         laptop.setNamaBarang("Laptop");
         laptop.setHargaBarang(5000000);
-        BarangService.getInstance().addBarang(laptop);
+        laptop.setDateCreated(new Date());
+        laptop.setLastModified(new Date());
+        barangDao.save(laptop);
 
         Barang mouse = new Barang();
         mouse.setKodeBarang("M0001");
         mouse.setNamaBarang("Mouse");
         mouse.setHargaBarang(100000);
-        BarangService.getInstance().addBarang(mouse);
+        laptop.setDateCreated(new Date());
+        laptop.setLastModified(new Date());
+        barangDao.save(mouse);
 
         Barang laptopgaming = new Barang();
         laptopgaming.setKodeBarang("LP002");
         laptopgaming.setNamaBarang("Laptop Gaming");
         laptopgaming.setHargaBarang(2000000);
-        BarangService.getInstance().addBarang(laptopgaming);
+        laptop.setDateCreated(new Date());
+        laptop.setLastModified(new Date());
+        barangDao.save(laptopgaming);
 
 
 
